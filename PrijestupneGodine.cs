2ee93cc4458778280bd46c6b1c8d1197744da9c3
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-28 12:18:53 +0100

    Add files via upload

---
+++ PrijestupneGodine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrijestupneGodine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1900; i <= 2100; i++)
+            {
+                if (i % 400 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else if(i % 4 == 0 && i % 100 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
